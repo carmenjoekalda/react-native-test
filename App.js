2bed99b801e1d5630d6217d74e20f679bd748345
Carmen Jõekalda <cjoekalda@gmail.com>
2025-08-31 02:11:51 +0300

    feat: Sign In page functionality

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import SignIn from './src/screens/SignIn';
+import Example from './src/screens/Example';
+
+const Stack = createNativeStackNavigator();
+
+export default () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{ title: "Sign In" }}
+      />
+      <Stack.Screen
+        name="Example"
+        component={Example}
+        options={{ title: "Success!" }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
